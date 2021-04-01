9509e72805aa6b413ae808b2a038da20b7c50441
잘익은 망고쥬스 <43088187+Mango-Juice@users.noreply.github.com>
2021-04-01 14:58:29 +0900

    Update 04_가장_흔한_단어.py

--- Coding_Interview/01~10/04_가장_흔한_단어.py
+++ Coding_Interview/01~10/04_가장_흔한_단어.py
@@ -6,6 +6,8 @@ import re
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         pg: List(str) = re.sub('[^a-z]', ' ', paragraph.lower()).split()
+        #소문자로 다 바꾼 후 a-z가 아닌 문자열을 공백으로 바꾸고, 공백을 기준으로 split함.
+        
         used_words: List(str) = []
         most_common: str = ""
         most_common_count: int = 0
