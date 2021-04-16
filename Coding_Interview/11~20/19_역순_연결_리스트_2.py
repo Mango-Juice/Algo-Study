43bbf9f0a0311e6514943a3aa071c1175fc57b14
잘익은 망고쥬스 <43088187+Mango-Juice@users.noreply.github.com>
2021-04-16 20:50:49 +0900

    Create 19_역순_연결_리스트_2.py

---
+++ Coding_Interview/11~20/19_역순_연결_리스트_2.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        if left == right or not head:
+            return head
+        
+        result = head = ListNode(0, head)
+        
+        for _ in range(left-1):
+            head = head.next
+        end = head.next
+            
+        for _ in range(right - left):
+            tmp, head.next, end.next = head.next, end.next, end.next.next
+            head.next.next = tmp
+            
+        return result.next
