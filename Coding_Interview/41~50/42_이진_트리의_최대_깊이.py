3a9d56ae857183546d24c5f42564767647297cd3
잘익은 망고쥬스 <43088187+Mango-Juice@users.noreply.github.com>
2021-05-09 00:54:45 +0900

    Create 42_이진_트리의_최대_깊이.py

---
+++ Coding_Interview/41~50/42_이진_트리의_최대_깊이.py
@@ -0,0 +1,30 @@
+#이진 트리의 최대 깊이
+#https://leetcode.com/problems/maximum-depth-of-binary-tree
+
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        depth = 0
+        queue = deque([root])
+        
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return depth
