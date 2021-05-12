9f7553d137f26ef214d6a57f25bbc155b550fd85
잘익은 망고쥬스 <43088187+Mango-Juice@users.noreply.github.com>
2021-05-13 00:04:40 +0900

    Create 46_두_이진_트리의_병합.py

---
+++ Coding_Interview/41~50/46_두_이진_트리의_병합.py
@@ -0,0 +1,19 @@
+#두 이진 트리의 병합
+#https://leetcode.com/problems/merge-two-binary-trees
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if root1 and root2:
+            node = TreeNode(root1.val + root2.val)
+            node.left = self.mergeTrees(root1.left, root2.left)
+            node.right = self.mergeTrees(root1.right, root2.right)
+            return node
+        else:
+            return root1 or root2
