3b559a59e1dd51c3928497795c99dbbbb8ecad9d
Mango-Juice <wjsalsrb5@naver.com>
2021-07-31 15:45:52 +0900

    7/31

---
+++ Baekjoon/Bit_Masking/18119_단어_암기.cpp
@@ -0,0 +1,45 @@
+// https://www.acmicpc.net/problem/18119
+
+#include <stdio.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int words[10000] = { 0 };
+
+int main(int argc, char** argv) {
+	int i, j, n, m, command;
+	char target;
+	int remember = 0;
+	char tmp[1000];
+
+	scanf("%d %d", &n, &m);
+
+	for (i = 0; i < 26; i++) remember += (1 << i);
+
+	for (i = 0; i < n; i++) {
+		j = 0;
+		scanf("%s", tmp);
+
+		while (tmp[j] != '\0') {
+			int num = tmp[j++] - 'a';
+			words[i] |= (1 << num);
+		}
+	}
+
+	for (i = 0; i < m; i++) {
+		int answer = 0;
+		scanf("%d %c", &command, &target);
+		remember ^= (1 << ((int)target - 'a')); // command 상관없이 뒤집으면 됨 (기억했었음/잊었었음이 보장되므로)
+		
+		for (j = 0; j < n; j++) {
+			if ((words[j] & remember) == words[j]) answer++;
+		}
+
+		printf("%d\n", answer);
+	}
+
+	return 0;
+}
