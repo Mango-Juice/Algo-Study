a9350761a7762e72ce08aae10079859c1453264a
Mango-Juice <wjsalsrb5@naver.com>
2021-09-20 14:49:25 +0900

    9/20

---
+++ Baekjoon/Divide_and_Conquer/5904_Moo_게임.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define FAST_IO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#include <iostream>
+
+using namespace std;
+
+int n, cnt = 0;
+
+void print(char a) {
+    cout << a;
+    exit(0);
+}
+
+void solution(int a) {
+    if (a == 1) {
+        if (cnt + 1 == n) print('m');
+        else if (n - cnt <= 3) print('o');
+        cnt += 3;
+        return;
+    }
+
+    solution(a - 1);
+    if (++cnt == n) print('m');
+    for (int i = 0; i <= a; i++) if (++cnt == n) print('o');
+    solution(a - 1);
+}
+
+int main() {
+    FAST_IO;
+    cin >> n;
+    solution(30);
+    return 0;
+}
