1f932ade096d22f2f2a7aad1c16a3f0f2d5a2f1d
Mango-Juice <wjsalsrb5@naver.com>
2021-10-03 16:45:51 +0900

    10/3

---
+++ Baekjoon/Greedy/13904_과제.cpp
@@ -0,0 +1,38 @@
+// https://www.acmicpc.net/problem/13904
+
+#define FAST_IO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+pair<int, int> arr[1001];
+int visited[1002] = { false };
+
+bool compare(pair<int, int> a, pair<int, int>b) {
+    if (a.second == b.second) return a.first < b.second;
+    return a.second > b.second;
+}
+
+int main() {
+    int i, day, N, answer = 0;
+    FAST_IO;
+
+    cin >> N;
+    for (i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
+    sort(arr, arr + N, compare);
+
+    for (i = 0; i < N; i++) {
+        day = arr[i].first;
+        while (visited[day] && day >= 1) day--;
+        if (day >= 1) {
+            visited[day] = true;
+            answer += arr[i].second;
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
