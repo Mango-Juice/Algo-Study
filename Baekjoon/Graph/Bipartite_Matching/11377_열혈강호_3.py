d79ac6425661ddc409cfad2e984c2483ee10be1a
전민규 <wjsalsrb5@naver.com>
2024-04-23 13:52:10 +0000

    24/04/23
    열혈강호(이분 매칭)

---
+++ Baekjoon/Graph/Bipartite_Matching/11377_열혈강호_3.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+works = [list(map(int, input().split()))[1:] for _ in range(n)]
+assign = [-1] * (m + 1)
+visited = [False] * (m + 1)
+
+def dfs(target):
+    for work in works[target]:
+        if visited[work] :
+            continue
+        visited[work] = True
+        
+        if assign[work] < 0 or dfs(assign[work]):
+            assign[work] = target
+            return 1
+    
+    return 0
+
+def run(target):
+    global visited
+    visited = [False] * (m + 1)
+    return dfs(target)
+
+first = sum([run(i) for i in range(n)])
+second = sum([run(i) for i in range(n)])
+print(first + min(second, k))
